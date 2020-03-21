963cd9c552ee5a0bfcc220ee7cd05701d0067a45
Misael Paul Mamangun <paul@simpleapps.com>
2020-03-21 17:00:09 +0800

    add web using vue

---
+++ express/router/api/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router();
+
+router.use('/user', require('./user'));
+
+router.get('/', (req, res) => {
+    res.send({
+        url: [
+            'user'
+        ]
+    })
+});
+
+module.exports = router;
--- express/server.js
+++ express/server.js
@@ -5,18 +5,27 @@ const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
 
-const router = express.Router();
-router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
-});
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
+// const router = express.Router();
+// router.get('/', (req, res) => {
+//   res.writeHead(200, { 'Content-Type': 'text/html' });
+//   res.write('<h1>Hello from Express.js!</h1>');
+//   res.end();
+// });
+// router.get('/another', (req, res) => {
+//   res.send('sample')
+// });
+// router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+app.use('/api', require('./router/api'))
+
+// app.use('/.netlify/functions/server', router);  // path must route to lambda
+
+app.use( '/', express.static( path.join(__dirname, '../', '/dist') ) );
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../', '/dist/index.html'))
+})
 
 module.exports = app;
 module.exports.handler = serverless(app);
