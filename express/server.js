be075194d30d420a307cc897a41338f175eed89f
Misael Paul Mamangun <paul@simpleapps.com>
2020-03-22 15:26:27 +0800

    add cors

--- express/server.js
+++ express/server.js
@@ -5,6 +5,9 @@ const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
 
+const cors = require('cors');
+app.use(cors());
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}))
 
