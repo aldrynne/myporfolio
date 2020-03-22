156754d34990e1f1c3d0791443ad765fc2188d67
Misael Paul Mamangun <paul@simpleapps.com>
2020-03-22 14:24:48 +0800

    trial ulit

--- express/server.js
+++ express/server.js
@@ -8,27 +8,27 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}))
 
-// const router = express.Router();
-// router.get('/', (req, res) => {
-//   res.writeHead(200, { 'Content-Type': 'text/html' });
-//   res.write('<h1>Hello from Express.js!</h1>');
-//   res.end();
-// });
-// router.get('/another', (req, res) => {
-//   res.send('sample')
-// });
-// router.post('/', (req, res) => res.json({ postBody: req.body }));
+const router = express.Router();
+router.get('/', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>Hello from Express.js!</h1>');
+  res.end();
+});
+router.get('/another', (req, res) => {
+  res.send('sample')
+});
+router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 app.use('/api', require('./api'))
 
-// app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use('/.netlify/functions/server', router);  // path must route to lambda
 
 app.use( '/', express.static( path.join(__dirname, '../dist') ) );
-const router = express.Router();
-router.get('/*', (req, res) => {
+const router1 = express.Router();
+router1.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '../dist/index.html'))
 })
-app.use('/', router);
+app.use('/', router1);
 
 module.exports = app;
 module.exports.handler = serverless(app);
